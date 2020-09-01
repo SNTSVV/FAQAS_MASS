65b041c6940c61cb9859177997baeb5cf6f2e358
Oscar Cornejo <oscar.cornejo@uni.lu>
2020-09-01 11:04:20 +0000

    mutation libutil

---
+++ FAQASOptimizations/FAQASMutation/launcher_libutil.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+HOME=/home/gsl                                                                                                       
+LIBUTIL=$HOME/libutil
+LIBUTIL_SRC=$LIBUTIL/src
+LIBUTIL_TST=$LIBUTIL/tst
+LIBUTIL_COV=$LIBUTIL_TST
+SRC_MUTANTS=/opt/mutations/src-mutants
+BUILD_COMMAND=(./waf build --notests)
+EXECUTION_COMMAND=(./waf --alltests)
+FILTER_TST="can_sniffer|rtc|util-app"
+EXEC_DIR=$HOME/test_runs
+TIMEOUT=250
+
+MUTATION=/opt/srcirorfaqas/FAQASOptimizations/FAQASMutation/mutation.sh
+
+cd $LIBUTIL
+find . -name '*.gcda' -delete
+
+source $MUTATION $LIBUTIL $LIBUTIL_SRC $LIBUTIL_TST $LIBUTIL_COV $SRC_MUTANTS $BUILD_COMMAND $EXECUTION_COMMAND $FILTER_TST $EXEC_DIR $TIMEOUT 
