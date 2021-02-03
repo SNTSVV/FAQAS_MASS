0895436a7f057aae491b7f0d480301aacecf381d
Fabrizio Pastore <fabrizio.pastore@uni.lu>
2021-02-03 12:11:05 +0100

    improved test suite

---
+++ mutator/python/executeTest.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+
+
+TESTFOLDER=$1
+curTest=$2
+FAULTMODEL=$3
+
+python generateDataMutator.py int $FAULTMODEL
+mv FAQAS_dataDrivenMutator.h $TESTFOLDER
+
+
+pushd $TESTFOLDER
+outFile=${curTest}.out
+compilerOutFile=${curTest}.compile.out
+rm $outFile
+
+pwd
+
+operations=`grep 'MUTATIONOPT=' FAQAS_dataDrivenMutator.h | tr '/' ' ' | awk -F= '{print $2}'`
+echo $operations
+x=-1
+while [ $x -le $operations ]; do
+    g++ -DMUTATIONOPT=$x ${curTest}.c -o main_$x >> $compilerOutFile 2>&1
+    ./main_$x >> $outFile 2>&1
+    echo "=====" >> $outFile 2>&1
+
+    x=$((x+1))
+done
+
+diff $outFile expected.out
+
+if [ $? -eq 0 ]; then
+    echo PASSED 
+else 
+    echo FAILED; 
+fi
+
+popd
--- mutator/python/runTest1.sh
+++ mutator/python/runTest1.sh
@@ -1,37 +1,9 @@
-#!/bin/bash
+#/bin/bash
 
-python generateDataMutator.py int faultModel.csv
-mv FAQAS_dataDrivenMutator.h tests/test1/
-
-
-
-#
-#	Test the fault model SIZE_IfHK
-#
-
-pushd tests/test1/
+TESTFOLDER=tests/test1/
 curTest="test1"
-outFile=${curTest}.out
-compilerOutFile=${curTest}.compile.out
-rm $outFile
+FAULTMODEL=faultModel1.csv
 
 
-operations=`grep 'MUTATIONOPT=' FAQAS_dataDrivenMutator.h | tr '/' ' ' | awk -F= '{print $2}'`
-echo $operations
-x=-1
-while [ $x -lt $operations ]; do
-    g++ -DMUTATIONOPT=$x test1.c -o main_$x >> $compilerOutFile 2>&1
-    ./main_$x >> $outFile 2>&1
-    echo "=====" >> $outFile 2>&1
+bash executeTest.sh "$TESTFOLDER" "$curTest" "$FAULTMODEL"
 
-    x=$((x+1))
-done
-
-diff $outFile expected.out
-
-if [ $? -eq 0 ]; then
-    echo PASSED 
-else 
-    echo FAILED; 
-fi
-popd
