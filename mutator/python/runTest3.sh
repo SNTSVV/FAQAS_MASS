bb4ca754d61ba67f27499d381f860fd7f05d8d86
Enrico <enrivigano@gmail.com>
2021-02-04 15:05:08 +0100

    Added test for INV operator

---
+++ mutator/python/runTest3.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+TESTFOLDER=tests/test3/
+curTest="test3"
+FAULTMODEL=faultModel3.csv
+
+
+bash executeTest.sh "$TESTFOLDER" "$curTest" "$FAULTMODEL"
--- mutator/python/tests/test3/test3.c
+++ mutator/python/tests/test3/test3.c
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <assert.h>     /* assert */
 #include "FAQAS_dataDrivenMutator.h"
 
+//this test will be used for INV operators
 
 int mutate( std::vector<int> *v, FaultModel *fm ){
     return _FAQAS_mutate(v->data(),fm);
 }
 
- 
+
 int main()
 {
     // Create a vector containing integers
@@ -21,17 +22,33 @@ int main()
     v.push_back(4);
     v.push_back(5);
 
+
+    std::vector<int> n;
+
+    n=v;
+
+    int position = _FAQAS_selectItem();
+    
+
     //MANUALLY ADDED PROBE
     FaultModel *fm = _FAQAS_IfHK_FM();
-    mutate( &v, fm );    
+    mutate( &v, fm );
     //MANUALLY ADDED PROBE END
 
 
-    data = v->data();
 
-    assert( data.at(2) != 2 )
-    assert( data.at(2) < 10 )
-    assert( data.at(2) > 0 )
+     // printf("********************************************\n");
+     // printf("this is the position:%i\n", position);
+     // printf("********************************************\n");
+    // printf("this is v(1):%i\n", v.at(1));
+    // printf("********************************************\n");
+
+
+    assert( v.at(position) != n.at(position));
+    assert( v.at(position) <= 5 );
+    assert( v.at(position) >= -5 );
+
+
 
     return 0;
 }
