dae804830891efb666698497fbb995d44c0bf3b5
Enrico <enrivigano@gmail.com>
2021-02-18 18:13:33 +0100

    new test (work in progress)

---
+++ mutator/python/NewTest/SUT.c
@@ -0,0 +1,418 @@
+#include "FAQAS_dataDrivenMutator.h"
+// #include <assert.h> /* assert */
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+int mutate( std::vector<long long int> *v, FaultModel *fm ){
+    return _FAQAS_mutate(v->data(),fm);
+}
+
+void double_push_back(std::vector<long long int> *v, double val) {
+
+  long long int tmp = 0;
+
+  memcpy(&tmp, &val, sizeof(double));
+
+  v->push_back(tmp);
+}
+
+void float_push_back(std::vector<long long int> *v, float val) {
+  long long int tmp = 0;
+
+  memcpy(&tmp, &val, sizeof(float));
+
+  v->push_back(tmp);
+}
+
+void int_push_back(std::vector<long long int> *v, int val) {
+  long long int tmp = 0;
+
+  memcpy(&tmp, &val, sizeof(int));
+
+  v->push_back(tmp);
+}
+
+std::vector<int> vectorA;
+
+std::vector<float> vectorB;
+
+std::vector<double> vectorC;
+
+std::vector<long long int> bufferA;
+std::vector<long long int> bufferB;
+std::vector<long long int> bufferC;
+std::vector<long long int> bufferMain;
+
+std::vector<int> minA;
+std::vector<int> maxA;
+std::vector<float> minB;
+std::vector<float> maxB;
+std::vector<double> minC;
+std::vector<double> maxC;
+
+void sensorA() {
+
+  // legge il vettore degli int
+
+  bufferA.clear();
+  int position=0;
+  while (position<5){
+    int_push_back(&bufferA, vectorA[position]);
+    position=position+1;
+  }
+
+  //PROBE
+  int mutate(&bufferA,fm)
+  //END OF THE PROBE
+
+}
+
+void sensorB() {
+
+  // legge il vettore dei float
+
+  bufferB.clear();
+  int position=0;
+  while (position<5) {
+    float_push_back(&bufferB, vectorB[position]);
+    position=position+1;
+  }
+}
+
+void sensorC() {
+
+  // legge il vettore dei double
+  bufferC.clear();
+  int position=0;
+  while (position<5){
+    double_push_back(&bufferC, vectorC[position]);
+    position=position+1;
+  }
+}
+
+void actuatorA() {
+  // riceve un vettore di zeri e di valori che indica su quale variabile agire
+  // con 3/5/30/50
+  int add = 0;
+  memcpy(&add, &bufferMain[0], sizeof(add));
+  vectorA[0] = vectorA[0] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[1], sizeof(add));
+  vectorA[1] = vectorA[1] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[2], sizeof(add));
+  vectorA[2] = vectorA[2] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[3], sizeof(add));
+  vectorA[3] = vectorA[3] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[4], sizeof(add));
+  vectorA[4] = vectorA[4] + add;
+  bufferMain.clear();
+}
+
+void actuatorB() {
+  // riceve un vettore di zeri e di valori che indica su quale variabile agire
+  // con 3/5/30/50
+  int add = 0;
+  memcpy(&add, &bufferMain[0], sizeof(add));
+  vectorB[0] = vectorB[0] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[1], sizeof(add));
+  vectorB[1] = vectorB[1] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[2], sizeof(add));
+  vectorB[2] = vectorB[2] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[3], sizeof(add));
+  vectorB[3] = vectorB[3] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[4], sizeof(add));
+  vectorB[4] = vectorB[4] + add;
+  bufferMain.clear();
+}
+
+void actuatorC() {
+  // riceve un vettore di zeri e di valori che indica su quale variabile agire
+  // con 3/5/30/50
+  int add = 0;
+  memcpy(&add, &bufferMain[0], sizeof(add));
+  vectorC[0] = vectorC[0] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[1], sizeof(add));
+  vectorC[1] = vectorC[1] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[2], sizeof(add));
+  vectorC[2] = vectorC[2] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[3], sizeof(add));
+  vectorC[3] = vectorC[3] + add;
+  add = 0;
+  memcpy(&add, &bufferMain[4], sizeof(add));
+  vectorC[4] = vectorC[4] + add;
+  bufferMain.clear();
+}
+
+int getCorrection(int error) {
+
+  int absoluteError = abs(error);
+  // e in base a quanto é sceglie il comando da applicare
+  int absCorrection = 0;
+
+  if (absoluteError == 0) {
+    absCorrection = 0;
+  }
+
+  else if (absoluteError > 0 && absoluteError < 5) {
+    absCorrection = 1;
+  }
+
+  else if (absoluteError >= 5 && absoluteError < 10) {
+    absCorrection = 5;
+  }
+
+  else if (absoluteError >= 10 && absoluteError < 50) {
+    absCorrection = 10;
+  }
+
+  else {
+    absCorrection = 100;
+  }
+
+  int correction;
+
+  if (error <= 0) {
+
+    correction = -absCorrection;
+  }
+
+  if (error > 0) {
+    correction = absCorrection;
+  }
+
+  return correction;
+}
+
+
+
+int main() {
+
+  //questo poi sarà preso da un csv
+
+  vectorA.clear();
+  vectorA.push_back(103);
+  vectorA.push_back(-223);
+  vectorA.push_back(532);
+  vectorA.push_back(87);
+  vectorA.push_back(-278);
+
+
+
+  maxA.push_back(50);
+  maxA.push_back(50);
+  maxA.push_back(50);
+  maxA.push_back(50);
+  maxA.push_back(50);
+
+  minA.push_back(-20);
+  minA.push_back(-20);
+  minA.push_back(-20);
+  minA.push_back(-20);
+  minA.push_back(-20);
+
+
+  vectorB.clear();
+  vectorB.push_back(103.2);
+  vectorB.push_back(-134.78);
+  vectorB.push_back(532.897);
+  vectorB.push_back(-23.7);
+  vectorB.push_back(100);
+
+  maxB.push_back(50);
+  maxB.push_back(50);
+  maxB.push_back(50);
+  maxB.push_back(50);
+  maxB.push_back(50);
+
+  minB.push_back(-20);
+  minB.push_back(-20);
+  minB.push_back(-20);
+  minB.push_back(-20);
+  minB.push_back(-20);
+
+  vectorC.push_back(188.99);
+  vectorC.push_back(178.678);
+  vectorC.push_back(-567);
+  vectorC.push_back(444.56);
+  vectorC.push_back(-42);
+
+  maxC.push_back(50);
+  maxC.push_back(50);
+  maxC.push_back(50);
+  maxC.push_back(50);
+  maxC.push_back(50);
+
+  minC.push_back(-20);
+  minC.push_back(-20);
+  minC.push_back(-20);
+  minC.push_back(-20);
+  minC.push_back(-20);
+
+  // // MANUALLY ADDED PROBE
+  // FaultModel *fm = _FAQAS_IfHK_FM();
+  // mutate(&v, fm);
+  // // MANUALLY ADDED PROBE END
+
+  printf("vectorA before\n");
+
+  for(std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end(); ++it) {
+    std::cout << *it << '\n';
+  }
+
+  printf("vectorB before\n");
+
+  for(std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end(); ++it) {
+    std::cout << *it << '\n';
+  }
+
+  printf("vectorC before\n");
+
+  for(std::vector<double>::iterator it = vectorC.begin(); it != vectorC.end(); ++it) {
+    std::cout << *it << '\n';
+  }
+
+  int cycles = 10;
+
+  while (cycles > 0) {
+
+    // prende il vettore dai sensori e legge le variabili int
+
+    bufferMain.clear();
+
+    sensorA();
+
+    int variablePosition;
+
+    variablePosition=0;
+    while(variablePosition<5) {
+
+      // prende il range, fa la media e lo considera il suo obiettivo
+
+      int target = (int)((minA[variablePosition] + maxA[variablePosition]) / 2);
+
+      // obbiettivo-variabile e ottiene l'errore
+
+      int variable = 0;
+      memcpy(&variable, &bufferA[variablePosition], sizeof(variable));
+
+      int error = (int)(target - variable);
+
+      int u = getCorrection(error);
+
+      int_push_back(&bufferMain, u);
+      variablePosition=variablePosition+1;
+    }
+
+    actuatorA();
+
+    bufferMain.clear();
+
+    sensorB();
+
+    variablePosition=0;
+    while(variablePosition<5) {
+
+      // prende il range, fa la media e lo considera il suo obiettivo
+
+      float target =
+          (float)((minB[variablePosition] + maxB[variablePosition]) / 2);
+
+      // obbiettivo-variabile e ottiene l'errore
+
+      float variable = 0;
+
+      memcpy(&variable, &bufferB[variablePosition], sizeof(variable));
+
+      int error = (int)(target - variable);
+
+      int u = getCorrection(error);
+
+      int_push_back(&bufferMain, u);
+      variablePosition=variablePosition+1;
+    }
+
+    actuatorB();
+
+    bufferMain.clear();
+
+    sensorC();
+
+    variablePosition=0;
+    while(variablePosition<5) {
+
+      // prende il range, fa la media e lo considera il suo obiettivo
+
+      double target =
+          (double)((minC[variablePosition] + maxC[variablePosition]) / 2);
+
+      // obbiettivo-variabile e ottiene l'errore
+
+      double variable = 0;
+
+      memcpy(&variable, &bufferC[variablePosition], sizeof(variable));
+
+      int error = (int)(target - variable);
+
+      int u = getCorrection(error);
+
+      int_push_back(&bufferMain, u);
+      variablePosition=variablePosition+1;
+    }
+
+    actuatorC();
+
+    printf("Remaining cycles: %d\n", cycles);
+
+    printf("vectorA\n");
+
+    for(std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end(); ++it) {
+      std::cout << *it << '\n';
+    }
+
+    printf("vectorB\n");
+
+    for(std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end(); ++it) {
+      std::cout << *it << '\n';
+    }
+
+    printf("vectorC\n");
+
+    for(std::vector<double>::iterator it = vectorC.begin(); it != vectorC.end(); ++it) {
+      std::cout << *it << '\n';
+    }
+
+    cycles=cycles-1;
+  }
+
+  printf("vectorA after\n");
+
+  for(std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end(); ++it) {
+    std::cout << *it << '\n';
+  }
+
+  printf("vectorB after\n");
+
+  for(std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end(); ++it) {
+    std::cout << *it << '\n';
+  }
+
+  printf("vectorC after\n");
+
+  for(std::vector<double>::iterator it = vectorC.begin(); it != vectorC.end(); ++it) {
+    std::cout << *it << '\n';
+  }
+
+  return 0;
+}
---
+++ mutator/python/NewTest/generateDataMutator.py
@@ -0,0 +1,358 @@
+#
+# Copyright (c) University of Luxembourg 2020.
+# Created by Fabrizio PASTORE, fabrizio.pastore@uni.lu, SnT, 2020.
+# Modified by Oscar Eduardo CORNEJO OLIVARES, oscar.cornejo@uni.lu, SnT, 2020.
+#
+
+import sys
+
+import csv
+
+elements=-1
+positions={}
+operators={}
+operations={}
+faultModelsDef=""
+operatorsCount=0
+lastFM=""
+lastItem=-1
+sizeDef=""
+lastSpan=""
+fmID=0
+
+def newBF(item,_span,_type,_min,_max,_state,_value):
+    global operations
+    global faultModelsDef
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=BF;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].min="+_min+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].max="+_max+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].state="+_state+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].value="+_value+";\n"
+
+    operations[elements] = 0
+
+def newHV(item,_span,_type,_value):
+    global operations
+    global faultModelsDef
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=HV;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].value="+_value+";\n"
+
+    operations[elements] = 0
+
+
+def newVOR(item,_span,_type,_min,_max,_delta):
+    global operators
+    global operations
+    global elements
+    global faultModelsDef
+
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=VOR;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].min="+_min+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].max="+_max+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].delta="+_delta+";\n"
+
+    operations[elements] = 0
+    currentOperator = operators[elements]
+    currentItem = positions[elements]
+
+    elements += 1
+    operations[elements] = 1
+    positions[elements] = currentItem
+    operators[elements] = currentOperator
+
+
+def newVAT(item,_span,_type,_threshold,_delta):
+    global operators
+    global operations
+    global elements
+    global faultModelsDef
+
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=VAT;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].threshold="+_threshold+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].delta="+_delta+";\n"
+
+    operations[elements] = 0
+
+
+def newVBT(item,_span,_type,_threshold,_delta):
+    global operators
+    global operations
+    global elements
+    global faultModelsDef
+
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=VBT;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].threshold="+_threshold+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].delta="+_delta+";\n"
+
+    operations[elements] = 0
+
+
+def newIV(item,_span,_type,_value):
+    global operators
+    global operations
+    global elements
+    global faultModelsDef
+
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=IV;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].value="+_value+";\n"
+
+    operations[elements] = 0
+
+
+def newINV(item,_span,_type,_min,_max,_delta,_value):
+    global operators
+    global operations
+    global elements
+    global faultModelsDef
+
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=INV;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].min="+_min+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].max="+_max+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].delta="+_delta+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].value="+_value+";\n"
+
+    operations[elements] = 0
+
+
+def newSS(item,_span,_type,_threshold,_delta):
+    global operators
+    global operations
+    global elements
+    global faultModelsDef
+
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=SS;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].threshold="+_threshold+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].delta="+_delta+";\n"
+
+    operations[elements] = 0
+
+
+def newASA(item,_span,_type,_threshold,_delta,_value):
+    global operators
+    global operations
+    global elements
+    global faultModelsDef
+
+    faultModelsDef+="\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].type=ASA;\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].threshold="+_threshold+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].delta="+_delta+";\n"
+    faultModelsDef += "fm->items["+str(item)+"].operators["+str(operatorsCount)+"].value="+_value+";\n"
+
+    operations[elements] = 0
+
+
+def closeFaultModelsDef():
+    global lastItem
+    global operatorsCount
+    global faultModelsDef
+    global sizeDef
+
+    size=int(lastItem)+1
+    sizeDef += "#define SIZE_"+lastFM+" "+str(size)+"\n"
+
+    faultModelsDef+="return fm;\n"
+    faultModelsDef+="}\n"
+
+
+def closeOperators():
+    global lastItem
+    global lastType
+    global operatorsCount
+    global faultModelsDef
+    global lastSpan
+    faultModelsDef += "fm->items["+str(lastItem)+"].operatorsN="+str(operatorsCount+1)+";\n"
+    faultModelsDef += "fm->items["+str(lastItem)+"].span="+str(lastSpan)+";\n"
+    faultModelsDef += "fm->items["+str(lastItem)+"].type="+str(lastType)+";\n"
+
+def processRow(row):
+    print(row)
+    global lastFM
+    global lastItem
+    global lastType
+    global lastSpan
+    global positions
+    global operators
+    global operations
+    global operatorsCount
+    global elements
+    global faultModelsDef
+    global fmID
+
+    _p=0;
+    FM = row[_p]
+
+    _p=_p+1;
+    item = row[_p]
+
+    _p=_p+1;
+    _span = row[_p]
+
+    _p=_p+1;
+    _type = row[_p]
+
+    _p=_p+1;
+    FT = row[_p]
+    _p=_p+1;
+    _min=row[_p]
+    _p=_p+1;
+    _max=row[_p]
+    _p=_p+1;
+    _threshold=row[_p]
+    _p=_p+1;
+    _delta=row[_p]
+    _p=_p+1;
+    _state=row[_p]
+    _p=_p+1;
+    _value=row[_p]
+
+    elements+=1
+
+    positions[elements]=item
+
+    if FM == lastFM:
+        if lastItem == item:
+            operatorsCount+=1
+        else:
+            closeOperators()
+            operatorsCount=0
+    else:
+        if lastFM != "":
+            closeOperators()
+            closeFaultModelsDef()
+
+        lastItem=-1
+        faultModelsDef+="struct FaultModel* _FAQAS_"+FM+"_FM(){\n"
+        faultModelsDef+="FaultModel *fm = _FAQAS_create_FM(SIZE_"+FM+");\n"
+        faultModelsDef += "fm->ID = " + str(fmID) + ";\n"
+        fmID += 1
+
+    operators[elements]=operatorsCount
+
+    if FT == 'BF':
+            newBF(item,_span,_type,_min,_max,_state,_value)
+    if FT == 'HV':
+            newHV(item,_span,_type,_value)
+    if FT == 'VOR':
+            newVOR(item,_span,_type,_min,_max,_delta)
+    if FT == 'VAT':
+            newVAT(item,_span,_type,_threshold,_delta)
+    if FT == 'VBT':
+            newVBT(item,_span,_type,_threshold,_delta)
+    if FT == 'IV':
+            newIV(item,_span,_type,_value)
+    if FT == 'INV':
+            newINV(item,_span,_type,_min,_max,_delta,_value)
+    if FT == 'SS':
+            newSS(item,_span,_type,_threshold,_delta)
+    if FT == 'ASA':
+            newASA(item,_span,_type,_threshold,_delta,_value)
+
+    lastFM=FM
+    lastItem=item
+    lastSpan=_span
+    lastType=_type
+
+
+def generateSelectFunctionContent(array):
+    selectItem=""
+    for k in array:
+        selectItem+="if ( MUTATION == "+str(k)+" )\n"
+        selectItem+="    return "+str(array[k])+";\n"
+    return selectItem
+
+
+
+
+if __name__ == "__main__":
+	argv = len(sys.argv)
+	if ( argv != 3 ):
+		print("Usage: generateDataMutator.py <BufferType> <FaultModel.csv>")
+
+bufferType=sys.argv[1]
+fileName=sys.argv[2]
+
+with open(fileName) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print('Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            processRow(row)
+            line_count += 1
+
+closeOperators()
+closeFaultModelsDef()
+
+maxFMO="//max MUTATIONOPT="+str(elements)
+
+selectItem="int _FAQAS_selectItem(){\n"
+selectItem+=generateSelectFunctionContent(positions)
+selectItem+="return -999;\n"
+selectItem+="}\n"
+
+selectOperator="int _FAQAS_selectOperator(){\n"
+selectOperator+=generateSelectFunctionContent(operators)
+selectOperator+="return -999;\n"
+selectOperator+="}\n"
+
+selectOperations="int _FAQAS_selectOperation(){\n"
+selectOperations+=generateSelectFunctionContent(operations)
+selectOperations+="return -999;\n"
+selectOperations+="}\n"
+
+outfile = open("FAQAS_dataDrivenMutator.h", "wt")
+outfile.write(maxFMO)
+
+
+with open('DDB_TEMPLATE_header.c', 'r') as tfile:
+    data = tfile.read().replace('BUFFER_TYPE', bufferType )
+    outfile.write(data)
+    tfile.close()
+
+sizeDef+="\n\n"
+outfile.write(sizeDef)
+
+faultModelsDef+="\n\n"
+outfile.write(faultModelsDef)
+
+outfile.write(selectItem)
+outfile.write(selectOperator)
+outfile.write(selectOperations)
+
+#TODO: we need to add an option to specify the number of mutations to apply
+outfile.write("\n\n#define APPLY_ONE_MUTATION 0\n\n")
+
+
+outfile.write("int FAQAS_fmCov;\n")
+outfile.write("void _FAQAS_fmCoverage(int fm){\n")
+outfile.write("    switch (fm){\n")
+for x in range(fmID):
+	line = "    case {}:\n"
+	outfile.write(line.format(fmID))
+	outfile.write("    FAQAS_fmCov++;\n")
+	outfile.write("    break;\n")
+
+
+
+outfile.write("    default:\n")
+outfile.write("    break;\n")
+outfile.write("    }\n")
+outfile.write("}\n")
+
+with open('DDB_TEMPLATE_footer.c', 'r') as tfile:
+    data = tfile.read().replace('BUFFER_TYPE', bufferType )
+    outfile.write(data)
+    tfile.close()
+
+outfile.close()
