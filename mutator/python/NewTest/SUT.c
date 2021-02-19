19784bb8763d5b8d7e31b59ca52daa2e7c27c565
Enrico <enrivigano@gmail.com>
2021-02-19 12:25:04 +0100

    updates on the new test

--- mutator/python/NewTest/SUT.c
+++ mutator/python/NewTest/SUT.c
@@ -1,13 +1,13 @@
-#include "FAQAS_dataDrivenMutator.h"
+// #include "FAQAS_dataDrivenMutator.h"
 // #include <assert.h> /* assert */
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 
-int mutate( std::vector<long long int> *v, FaultModel *fm ){
-    return _FAQAS_mutate(v->data(),fm);
-}
+// int mutate(std::vector<long long int> *v, FaultModel *fm) {
+//   return _FAQAS_mutate(v->data(), fm);
+// }
 
 void double_push_back(std::vector<long long int> *v, double val) {
 
@@ -57,16 +57,15 @@ void sensorA() {
   // legge il vettore degli int
 
   bufferA.clear();
-  int position=0;
-  while (position<5){
+  int position = 0;
+  while (position < 5) {
     int_push_back(&bufferA, vectorA[position]);
-    position=position+1;
+    position = position + 1;
   }
 
-  //PROBE
-  int mutate(&bufferA,fm)
-  //END OF THE PROBE
-
+  // // PROBE
+  // int mutate(&bufferA, fm)
+  // // END OF THE PROBE
 }
 
 void sensorB() {
@@ -74,10 +73,10 @@ void sensorB() {
   // legge il vettore dei float
 
   bufferB.clear();
-  int position=0;
-  while (position<5) {
+  int position = 0;
+  while (position < 5) {
     float_push_back(&bufferB, vectorB[position]);
-    position=position+1;
+    position = position + 1;
   }
 }
 
@@ -85,10 +84,10 @@ void sensorC() {
 
   // legge il vettore dei double
   bufferC.clear();
-  int position=0;
-  while (position<5){
+  int position = 0;
+  while (position < 5) {
     double_push_back(&bufferC, vectorC[position]);
-    position=position+1;
+    position = position + 1;
   }
 }
 
@@ -195,11 +194,9 @@ int getCorrection(int error) {
   return correction;
 }
 
-
-
 int main() {
 
-  //questo poi sarà preso da un csv
+  // questo poi sarà preso da un csv
 
   vectorA.clear();
   vectorA.push_back(103);
@@ -208,8 +205,6 @@ int main() {
   vectorA.push_back(87);
   vectorA.push_back(-278);
 
-
-
   maxA.push_back(50);
   maxA.push_back(50);
   maxA.push_back(50);
@@ -221,7 +216,6 @@ int main() {
   minA.push_back(-20);
   minA.push_back(-20);
   minA.push_back(-20);
-
 
   vectorB.clear();
   vectorB.push_back(103.2);
@@ -265,153 +259,474 @@ int main() {
   // mutate(&v, fm);
   // // MANUALLY ADDED PROBE END
 
+
+
+
+
+//**************************************************************************
+//**************************************************************************
+//**************************************************************************
+//**************************************************************************
+
+
+  int controlType = 2;
+  // 0=feedback loop: both sensors and actuators are active
+  // 1=feedforward: actuators are active but non sensors
+  // 2=monitor: only sensors are activated and control that numbers stay in the
+  // range
+
+
+//**************************************************************************
+//**************************************************************************
+//**************************************************************************
+//**************************************************************************
+
+
+
   printf("vectorA before\n");
 
-  for(std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end(); ++it) {
+  for (std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end();
+       ++it) {
     std::cout << *it << '\n';
   }
 
   printf("vectorB before\n");
 
-  for(std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end(); ++it) {
+  for (std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end();
+       ++it) {
     std::cout << *it << '\n';
   }
 
   printf("vectorC before\n");
 
-  for(std::vector<double>::iterator it = vectorC.begin(); it != vectorC.end(); ++it) {
+  for (std::vector<double>::iterator it = vectorC.begin(); it != vectorC.end();
+       ++it) {
     std::cout << *it << '\n';
   }
 
   int cycles = 10;
 
-  while (cycles > 0) {
+  //**********feedback****************************************************
 
-    // prende il vettore dai sensori e legge le variabili int
+  if (controlType == 0) {
 
-    bufferMain.clear();
+    while (cycles > 0) {
 
-    sensorA();
+      // prende il vettore dai sensori e legge le variabili int
 
-    int variablePosition;
+      bufferMain.clear();
 
-    variablePosition=0;
-    while(variablePosition<5) {
+      sensorA();
 
-      // prende il range, fa la media e lo considera il suo obiettivo
+      int variablePosition;
 
-      int target = (int)((minA[variablePosition] + maxA[variablePosition]) / 2);
+      variablePosition = 0;
+      while (variablePosition < 5) {
 
-      // obbiettivo-variabile e ottiene l'errore
+        // prende il range, fa la media e lo considera il suo obiettivo
 
-      int variable = 0;
-      memcpy(&variable, &bufferA[variablePosition], sizeof(variable));
+        int target =
+            (int)((minA[variablePosition] + maxA[variablePosition]) / 2);
 
-      int error = (int)(target - variable);
+        // obbiettivo-variabile e ottiene l'errore
 
-      int u = getCorrection(error);
+        int variable = 0;
+        memcpy(&variable, &bufferA[variablePosition], sizeof(variable));
 
-      int_push_back(&bufferMain, u);
-      variablePosition=variablePosition+1;
+        int error = (int)(target - variable);
+
+        int u = getCorrection(error);
+
+        int_push_back(&bufferMain, u);
+        variablePosition = variablePosition + 1;
+      }
+
+      actuatorA();
+
+      bufferMain.clear();
+
+      sensorB();
+
+      variablePosition = 0;
+      while (variablePosition < 5) {
+
+        // prende il range, fa la media e lo considera il suo obiettivo
+
+        float target =
+            (float)((minB[variablePosition] + maxB[variablePosition]) / 2);
+
+        // obbiettivo-variabile e ottiene l'errore
+
+        float variable = 0;
+
+        memcpy(&variable, &bufferB[variablePosition], sizeof(variable));
+
+        int error = (int)(target - variable);
+
+        int u = getCorrection(error);
+
+        int_push_back(&bufferMain, u);
+        variablePosition = variablePosition + 1;
+      }
+
+      actuatorB();
+
+      bufferMain.clear();
+
+      sensorC();
+
+      variablePosition = 0;
+      while (variablePosition < 5) {
+
+        // prende il range, fa la media e lo considera il suo obiettivo
+
+        double target =
+            (double)((minC[variablePosition] + maxC[variablePosition]) / 2);
+
+        // obbiettivo-variabile e ottiene l'errore
+
+        double variable = 0;
+
+        memcpy(&variable, &bufferC[variablePosition], sizeof(variable));
+
+        int error = (int)(target - variable);
+
+        int u = getCorrection(error);
+
+        int_push_back(&bufferMain, u);
+        variablePosition = variablePosition + 1;
+      }
+
+      actuatorC();
+
+      printf("Remaining cycles: %d\n", cycles);
+
+      printf("vectorA\n");
+
+      for (std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end();
+           ++it) {
+        std::cout << *it << '\n';
+      }
+
+      printf("vectorB\n");
+
+      for (std::vector<float>::iterator it = vectorB.begin();
+           it != vectorB.end(); ++it) {
+        std::cout << *it << '\n';
+      }
+
+      printf("vectorC\n");
+
+      for (std::vector<double>::iterator it = vectorC.begin();
+           it != vectorC.end(); ++it) {
+        std::cout << *it << '\n';
+      }
+
+      cycles = cycles - 1;
     }
 
-    actuatorA();
+    printf("vectorA after\n");
 
-    bufferMain.clear();
-
-    sensorB();
-
-    variablePosition=0;
-    while(variablePosition<5) {
-
-      // prende il range, fa la media e lo considera il suo obiettivo
-
-      float target =
-          (float)((minB[variablePosition] + maxB[variablePosition]) / 2);
-
-      // obbiettivo-variabile e ottiene l'errore
-
-      float variable = 0;
-
-      memcpy(&variable, &bufferB[variablePosition], sizeof(variable));
-
-      int error = (int)(target - variable);
-
-      int u = getCorrection(error);
-
-      int_push_back(&bufferMain, u);
-      variablePosition=variablePosition+1;
-    }
-
-    actuatorB();
-
-    bufferMain.clear();
-
-    sensorC();
-
-    variablePosition=0;
-    while(variablePosition<5) {
-
-      // prende il range, fa la media e lo considera il suo obiettivo
-
-      double target =
-          (double)((minC[variablePosition] + maxC[variablePosition]) / 2);
-
-      // obbiettivo-variabile e ottiene l'errore
-
-      double variable = 0;
-
-      memcpy(&variable, &bufferC[variablePosition], sizeof(variable));
-
-      int error = (int)(target - variable);
-
-      int u = getCorrection(error);
-
-      int_push_back(&bufferMain, u);
-      variablePosition=variablePosition+1;
-    }
-
-    actuatorC();
-
-    printf("Remaining cycles: %d\n", cycles);
-
-    printf("vectorA\n");
-
-    for(std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end(); ++it) {
+    for (std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end();
+         ++it) {
       std::cout << *it << '\n';
     }
 
-    printf("vectorB\n");
+    printf("vectorB after\n");
 
-    for(std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end(); ++it) {
+    for (std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end();
+         ++it) {
       std::cout << *it << '\n';
     }
 
-    printf("vectorC\n");
+    printf("vectorC after\n");
 
-    for(std::vector<double>::iterator it = vectorC.begin(); it != vectorC.end(); ++it) {
+    for (std::vector<double>::iterator it = vectorC.begin();
+         it != vectorC.end(); ++it) {
+      std::cout << *it << '\n';
+    }
+  }
+
+  //**********feedforward****************************************************
+
+  if (controlType == 1) {
+
+    // save the avriable vectors directly as initial initialConditions
+
+    std::vector<int> initialConditionsA = vectorA;
+    std::vector<float> initialConditionsB = vectorB;
+    std::vector<double> initialConditionsC = vectorC;
+
+    while (cycles > 0) {
+
+      // prende il vettore dai sensori e legge le variabili int
+
+      bufferMain.clear();
+
+      int variablePosition;
+
+      variablePosition = 0;
+      while (variablePosition < 5) {
+
+        // prende il range, fa la media e lo considera il suo obiettivo
+
+        int target =
+            (int)((minA[variablePosition] + maxA[variablePosition]) / 2);
+
+        // obbiettivo-variabile e ottiene l'errore
+
+        int error = (int)(target - initialConditionsA[variablePosition]);
+
+        int u = getCorrection(error);
+
+        int_push_back(&bufferMain, u);
+
+        initialConditionsA[variablePosition] =
+            initialConditionsA[variablePosition] + u;
+
+        variablePosition = variablePosition + 1;
+      }
+
+      actuatorA();
+
+      bufferMain.clear();
+
+      variablePosition = 0;
+      while (variablePosition < 5) {
+
+        // prende il range, fa la media e lo considera il suo obiettivo
+
+        float target =
+            (float)((minB[variablePosition] + maxB[variablePosition]) / 2);
+
+        // obbiettivo-variabile e ottiene l'errore
+
+        int error = (int)(target - initialConditionsB[variablePosition]);
+
+        int u = getCorrection(error);
+
+        int_push_back(&bufferMain, u);
+
+        initialConditionsB[variablePosition] =
+            initialConditionsB[variablePosition] + u;
+
+        variablePosition = variablePosition + 1;
+      }
+
+      actuatorB();
+
+      bufferMain.clear();
+
+      variablePosition = 0;
+      while (variablePosition < 5) {
+
+        // prende il range, fa la media e lo considera il suo obiettivo
+
+        double target =
+            (double)((minC[variablePosition] + maxC[variablePosition]) / 2);
+
+        // obbiettivo-variabile e ottiene l'errore
+
+        int error = (int)(target - initialConditionsC[variablePosition]);
+
+        int u = getCorrection(error);
+
+        int_push_back(&bufferMain, u);
+
+        initialConditionsC[variablePosition] =
+            initialConditionsC[variablePosition] + u;
+
+        variablePosition = variablePosition + 1;
+      }
+
+      actuatorC();
+
+      printf("Remaining cycles: %d\n", cycles);
+
+      printf("vectorA\n");
+
+      for (std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end();
+           ++it) {
+        std::cout << *it << '\n';
+      }
+
+      printf("vectorB\n");
+
+      for (std::vector<float>::iterator it = vectorB.begin();
+           it != vectorB.end(); ++it) {
+        std::cout << *it << '\n';
+      }
+
+      printf("vectorC\n");
+
+      for (std::vector<double>::iterator it = vectorC.begin();
+           it != vectorC.end(); ++it) {
+        std::cout << *it << '\n';
+      }
+
+      cycles = cycles - 1;
+    }
+
+    printf("vectorA after\n");
+
+    for (std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end();
+         ++it) {
       std::cout << *it << '\n';
     }
 
-    cycles=cycles-1;
+    printf("vectorB after\n");
+
+    for (std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end();
+         ++it) {
+      std::cout << *it << '\n';
+    }
+
+    printf("vectorC after\n");
+
+    for (std::vector<double>::iterator it = vectorC.begin();
+         it != vectorC.end(); ++it) {
+      std::cout << *it << '\n';
+    }
   }
 
-  printf("vectorA after\n");
+  //**********monitor****************************************************
 
-  for(std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end(); ++it) {
-    std::cout << *it << '\n';
-  }
+  if (controlType == 2) {
 
-  printf("vectorB after\n");
+    while (cycles > 0) {
 
-  for(std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end(); ++it) {
-    std::cout << *it << '\n';
-  }
+      // prende il vettore dai sensori e legge le variabili int
 
-  printf("vectorC after\n");
+      sensorA();
 
-  for(std::vector<double>::iterator it = vectorC.begin(); it != vectorC.end(); ++it) {
-    std::cout << *it << '\n';
+      int variablePosition;
+
+      variablePosition = 0;
+      while (variablePosition < 5) {
+
+        int variable = 0;
+        memcpy(&variable, &bufferA[variablePosition], sizeof(variable));
+
+        if (variable > maxA[variablePosition]) {
+          printf("variable A%d is too low\n", variablePosition);
+        }
+
+        else if (variable < minA[variablePosition]) {
+          printf("variable A%d is too high\n", variablePosition);
+        }
+
+        else {
+          printf("variable A%d is in range\n", variablePosition);
+        }
+
+        variablePosition = variablePosition + 1;
+      }
+
+      sensorB();
+
+      variablePosition = 0;
+      while (variablePosition < 5) {
+
+        float variable = 0;
+
+        memcpy(&variable, &bufferB[variablePosition], sizeof(variable));
+
+        if (variable > maxB[variablePosition]) {
+          printf("variable B%d is too low\n", variablePosition);
+        }
+
+        else if (variable < minB[variablePosition]) {
+          printf("variable B%d is too high\n", variablePosition);
+        }
+
+        else {
+          printf("variable B%d is in range\n", variablePosition);
+        }
+
+        variablePosition = variablePosition + 1;
+      }
+
+      sensorC();
+
+      variablePosition = 0;
+      while (variablePosition < 5) {
+
+        double variable = 0;
+
+        memcpy(&variable, &bufferC[variablePosition], sizeof(variable));
+
+        if (variable > maxC[variablePosition]) {
+          printf("variable C%d is too low\n", variablePosition);
+        }
+
+        else if (variable < minC[variablePosition]) {
+          printf("variable C%d is too high\n", variablePosition);
+        }
+
+        else {
+          printf("variable C%d is in range\n", variablePosition);
+        }
+
+        variablePosition = variablePosition + 1;
+      }
+
+      printf("Remaining cycles: %d\n", cycles);
+
+      printf("vectorA\n");
+
+      for (std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end();
+           ++it) {
+        std::cout << *it << '\n';
+      }
+
+      printf("vectorB\n");
+
+      for (std::vector<float>::iterator it = vectorB.begin();
+           it != vectorB.end(); ++it) {
+        std::cout << *it << '\n';
+      }
+
+      printf("vectorC\n");
+
+      for (std::vector<double>::iterator it = vectorC.begin();
+           it != vectorC.end(); ++it) {
+        std::cout << *it << '\n';
+      }
+
+      variablePosition = 0;
+
+      while (variablePosition < 5) {
+
+        vectorA[variablePosition] = vectorA[variablePosition] + 30;
+        vectorB[variablePosition] = vectorB[variablePosition] + 30;
+        vectorC[variablePosition] = vectorB[variablePosition] + 30;
+
+        variablePosition = variablePosition + 1;
+      }
+
+      cycles = cycles - 1;
+    }
+
+    printf("vectorA after\n");
+
+    for (std::vector<int>::iterator it = vectorA.begin(); it != vectorA.end();
+         ++it) {
+      std::cout << *it << '\n';
+    }
+
+    printf("vectorB after\n");
+
+    for (std::vector<float>::iterator it = vectorB.begin(); it != vectorB.end();
+         ++it) {
+      std::cout << *it << '\n';
+    }
+
+    printf("vectorC after\n");
+
+    for (std::vector<double>::iterator it = vectorC.begin();
+         it != vectorC.end(); ++it) {
+      std::cout << *it << '\n';
+    }
   }
 
   return 0;
