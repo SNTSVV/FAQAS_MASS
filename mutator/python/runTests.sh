88ff1c7dc415c01d888f5487bd0453e678664d1b
Enrico <enrivigano@gmail.com>
2021-02-26 13:30:24 +0100

    Update runTests.sh

--- mutator/python/runTests.sh
+++ mutator/python/runTests.sh
@@ -3,35 +3,36 @@ bash cleanTests.sh
 
 pushd tests
 
-bash runTest1.sh
-bash runTest2.sh
-bash runTest3.sh
-bash runTest4.sh
-bash runTest5.sh
-bash runTest6.sh
-bash runTest7.sh
-bash runTest8.sh
-bash runTest9.sh
-bash runTest10.sh
-bash runTest11.sh
-bash runTest12.sh
-bash runTest13.sh
-bash runTest14.sh
-bash runTest15.sh
-bash runTest16.sh
-bash runTest17.sh
-bash runTest18.sh
-bash runTest19.sh
-bash runTest20.sh
-bash runTest21.sh
-bash runTest22.sh
-bash runTest23.sh
-bash runTest24.sh
-bash runTest25.sh
-bash runTest26.sh
-bash runTest27.sh
-bash runTest28.sh
-bash runTest29.sh
+# bash runTest1.sh
+# bash runTest2.sh
+# bash runTest3.sh
+# bash runTest4.sh
+# bash runTest5.sh
+# bash runTest6.sh
+# bash runTest7.sh
+# bash runTest8.sh
+# bash runTest9.sh
+# bash runTest10.sh
+# bash runTest11.sh
+# bash runTest12.sh
+# bash runTest13.sh
+# bash runTest14.sh
+# bash runTest15.sh
+# bash runTest16.sh
+# bash runTest17.sh
+# bash runTest18.sh
+# bash runTest19.sh
+# bash runTest20.sh
+# bash runTest21.sh
+# bash runTest22.sh
+# bash runTest23.sh
+# bash runTest24.sh
+# bash runTest25.sh
+# bash runTest26.sh
+# bash runTest27.sh
+# bash runTest28.sh
+# bash runTest29.sh
+bash runTest30.sh
 bash runTest32.sh
 bash runTest33.sh
 bash runTest34.sh
