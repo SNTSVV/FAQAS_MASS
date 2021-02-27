91f53a0d191b594f744f95e28d245e6e256c603f
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-02-27 12:35:01 +0000

    Update underlying_test_generation.py

--- underlying_test_generation.py
+++ underlying_test_generation.py
@@ -5,3 +5,15 @@ Requires Python 3
 Use muteria for this
 TODO
 """
+
+import argparse
+
+def main():
+    """
+    Entrypoint
+    """
+    pass
+#~ def main()
+
+if __name__ == "__main__":
+    main()
