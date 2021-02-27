ef0e6cf2bfffca6e3704e9b8163412176e6819be
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-02-27 12:39:49 +0000

    Add new file

---
+++ pre_semu.py
@@ -0,0 +1,15 @@
+#! /user/bin/env python
+
+"""
+Implement meta-mu source generator
+- Require libclang (https://pypi.org/project/libclang/)
+"""
+
+import argparse
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
