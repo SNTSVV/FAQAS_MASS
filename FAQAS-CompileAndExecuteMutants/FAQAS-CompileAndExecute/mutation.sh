e68156fecda8cea2ce10b1b1450920a21f09cb51
Oscar Cornejo <oscar.cornejo@uni.lu>
2021-03-02 14:59:10 +0000

    installation files

--- FAQAS-CompileAndExecuteMutants/FAQAS-CompileAndExecute/mutation.sh
+++ FAQAS-CompileAndExecuteMutants/FAQAS-CompileAndExecute/mutation.sh
@@ -1,44 +1,7 @@
 #!/bin/bash
 
-backup_tst_coverage() {
-    tst=$1
-
-    mutant_src_name=$2
-    mutant_exec_path=$3
-   
-    cd $HOME/blts_workspace 
-    rm -rf $tst/Reports/Data/libm
-    GZIP=-9 tar czf ${tst}.tar.gz $tst/Reports/Coverage/Data/${mutant_src_name}.gc*
-    mv ${tst}.tar.gz $mutant_exec_path/coverage
-}
-
-run_tst_case() {
-
-    tst_name=$1
-    tst=$PROJ_TST/$tst_name.xml
-
-    echo $tst_name $tst
-    
-    # run the test case
-    cd /home/mlfs/blts_workspace
-    $HOME/blts_install/bin/blts_app -gcrx $tst_name -b coverage --nocsv -s $tst
-
-    # define if test case execution passed or failed
-    summaryreport=$tst_name/Reports/SessionSummaryReport.xml
-    originalreport=$HOME/unit-reports/$summaryreport
-
-    test_cases_failed=$(xmllint --xpath "//report_summary/test_set_summary/test_cases_failed/text()" $summaryreport)
-    o_test_cases_failed=$(xmllint --xpath "//report_summary/test_set_summary/test_cases_failed/text()" $originalreport)
-
-    echo "comparing with original execution" 2>&1 | tee -a $MUTANT_LOGFILE
-    echo $test_cases_failed $o_test_cases_failed 2>&1 | tee -a $MUTANT_LOGFILE
-
-    if [ "$test_cases_failed" != "$o_test_cases_failed" ]; then
-        return 1
-    else
-        return 0
-    fi
-}
+# loading functions backup_tst_coverage and run_tst_case
+. $APP_RUN_DIR/mutation_additional_functions.sh
 
 # Returns 124 if timed out, otherwise it returns the exit code of command
 ctimeout() {
