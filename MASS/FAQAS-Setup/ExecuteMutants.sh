df62b3ca830f6584bab2db53fee3ad2f3eac822a
Oscar Cornejo <oscar.cornejo@uni.lu>
2021-03-04 12:02:59 +0100

    adapting to HPC execution
    Former-commit-id: e1168bc4c1c69dc228707407f64d65b2b507301d

--- MASS/FAQAS-Setup/ExecuteMutants.sh
+++ MASS/FAQAS-Setup/ExecuteMutants.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# 1. FAQAS-GeneratePrioritizedTestSuite
-
 cd INSTALL_DIR_REPL
 . ./mass_conf.sh
-
-source $MASS/FAQAS-CompileAndExecuteMutants/FAQAS-GeneratePrioritizedTestSuite/FAQAS-GeneratePrioritizedTestSuite.sh
 
 # 2. FAQAS-CompileAndExecute
 
 source $MASS/FAQAS-CompileAndExecuteMutants/FAQAS-CompileAndExecute/FAQAS-CompileAndExecute.sh $SAMPLING $RATE $PRIORITIZED
 
-# 3. FAQAS-IdentifyEquivalentAndRedundantMutants
-
-source $MASS/FAQAS-CompileAndExecuteMutants/FAQAS-IdentifyEquivalentAndRedundantMutants/FAQAS-IdentifyEquivalentAndRedundantMutants.sh
---
+++ MASS/FAQAS-Setup/IdentifyEquivalents.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd INSTALL_DIR_REPL
+. ./mass_conf.sh
+
+# 3. FAQAS-IdentifyEquivalentAndRedundantMutants
+
+source $MASS/FAQAS-CompileAndExecuteMutants/FAQAS-IdentifyEquivalentAndRedundantMutants/FAQAS-IdentifyEquivalentAndRedundantMutants.sh
