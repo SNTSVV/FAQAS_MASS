3f5b9334a17810c124c9aea3fd43bd9148ea27f6
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-03-10 23:40:28 +0000

    Update pre_semu.py

--- pre_semu.py
+++ pre_semu.py
@@ -2,11 +2,17 @@
 
 """
 Implement meta-mu source generator
-- Require srcML (https://www.srcml.org/)
+TODO: Implement it as following:
+    - Modify MASS so that when generating each mutant file, 
+        another file is generated, following the Meta-mu format
+        but having that single mutant (and only original in switch).
+        * Use same tmp variable id accross mutants (keep a map between node and tmp id)
+        * The whole code that changes accross mutants on the same statement (around the switch part)
+           Must be surrounded by special comments, or just do the diff of mutants on same line to figure out the change
+    - In this module, implements some code to unify the generated single mutant meta-mu files
 """
 
 import argparse
-import xml.etree.ElementTree as ET
 
 def main():
     pass
