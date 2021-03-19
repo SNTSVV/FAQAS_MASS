5ba85244cf911705f50c0b137a4c991c07bbbb74
Oscar Cornejo <oscar.cornejo@uni.lu>
2021-03-19 10:20:41 +0100

    missed deleting commands from prepare sut script

--- FAQAS-Setup/PrepareSUT.sh
+++ FAQAS-Setup/PrepareSUT.sh
@@ -45,22 +45,3 @@ cd INSTALL_DIR_REPL
 #    source $MASS/FAQAS-GenerateCodeCoverageMatrixes/FAQAS-CollectCodeCoverage.sh $tst_filename_wo_xml "$(($end-$start))"
 #done
 
-# 4. Execute FAQAS-GenerateCodeCoverageMatrixes
-
-# Generate global code coverage matrix 
-echo "generating global code coverage matrix"
-source $MASS/FAQAS-GenerateCodeCoverageMatrixes/FAQAS-GenerateCodeCoverageMatrixes.sh 1
-
-# Generate individual code coverage matrices
-echo "generating individual code coverage matrices"
-source $MASS/FAQAS-GenerateCodeCoverageMatrixes/FAQAS-GenerateCodeCoverageMatrixes.sh 0
-
-# Generate SRCIRor configuration file
-echo "generating SRCIRor coverage file"
-source $MASS/FAQAS-GenerateCodeCoverageMatrixes/FAQAS-GenerateSRCIRorConf.sh 
-
-# 5. Generate mutants
-
-# commands_json must contain full paths
-source $MASS/FAQAS-GenerateMutants/FAQAS-GenerateMutants.sh 
-
