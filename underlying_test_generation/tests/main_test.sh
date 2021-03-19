d4a856b89fe86a95d9c867a66d9283b11e3a0d96
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-03-19 23:40:07 +0000

    Update underlying_test_generation/tests/main_test.sh, underlying_test_generation/tests/klee-semu, underlying_test_generation/tests/bcmock.bc files

---
+++ underlying_test_generation/tests/main_test.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# run the test
+
+set -u
+
+error_exit(){
+    echo "ERROR: $1"
+    exit 1
+}
+
+topdir=$(dirname $(readlink -f $0))
+
+test_subject=$topdir/../main.py
+test -f $test_subject || error_exit "test_subject missing ($test_subject)"
+
+tmpout=$topdir/tmpoutput.tmp
+test -d $tmpout && rm -rf $tmpout
+mkdir $tmpout
+
+export PATH=$topdir:$PATH
+
+python $test_subject $topdir/bcmock.bc --output_top_directory $tmpout --clear_existing || error_exit "call to test subject failed"
+
