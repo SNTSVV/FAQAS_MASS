4c43b9f9e1cb99b41d40196e6e7d01bf45ec2784
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-03-24 00:47:20 +0000

    Update pre_semu/main.py

--- pre_semu/main.py
+++ pre_semu/main.py
@@ -13,6 +13,41 @@ TODO: Implement it as following:
 """
 
 import argparse
+import collections
+import difflib
+
+StmtInfo = collections.namedtuple("StmtInfo", ["start_index", "length"])
+
+def get_stmt_list (original_src_file, mutant_src_file_list):
+    """
+    take original and SDL mutants, anf find stmts
+    """
+    seq_matcher = SequenceMatcher()
+    with open(original_src_file) as f:
+        orig_str = f.read()
+    seq_matcher.set_seq1(orig_str)
+    stmt_list = []
+    for mutant_src_file in mutant_src_file_list:
+        with open(mutant_src_file) as f:
+            mutant_str = f.read()
+        # compute the deleted statement location
+        seq_matcher.set_seq2(mutant_str)
+        match_blocks = seq_matcher.get_matching_blocks()
+        orig_idx = mut_idx = 0
+        for mb in match_blocks:
+            # a is orig, b is mut
+            assert mb.b == mut_idx, "unexpected gap in mutant-orig at index".format(mut_idx)
+            if orig_idx < mb.a and mb.size > 0:
+                stmt_list.append(
+                    StmtInfo(
+                        start_index=orig_idx,
+                        length=(mb.a - orig_idx)
+                    )
+                )
+            orig_idx = mb.a + mb.size
+            mut_idx = mb.b + mb.size
+    return stmt_list
+        
 
 def main():
     # Call SDL generation
