6935322f9609d967996f16370a8a50fc51740cc3
Oscar Cornejo <oscar.cornejo@uni.lu>
2021-04-06 18:52:35 +0000

    mass: sdl bug fix

--- FAQAS-CompileAndExecuteMutants/FAQAS-IdentifyEquivalentAndRedundantMutants/distance.py
+++ FAQAS-CompileAndExecuteMutants/FAQAS-IdentifyEquivalentAndRedundantMutants/distance.py
@@ -62,18 +62,15 @@ def get_distance(testA, testB):
 
     global lineNumber
     if operator == "SDL":
-        covBList[lineNumber] = covAList[lineNumber]
-        
-        if len(covAList) != len(covBList):
-            lineToFix = lineNumber + 1
+        if len(covAList) == len(covBList):
+            del covAList[lineNumber]
+            del covBList[lineNumber]
+        else:
             while len(covAList) > len(covBList):
-                covBList.insert(lineToFix, covAList[lineToFix])
-                lineToFix += 1
-    else:
-        if len(covAList) != len(covBList):
-            while len(covAList) != len(covBList):
-                if len(covAList) > len(covBList):
-                    covBList.insert(lineNumber, int(0))
+                del covAList[lineNumber]
+            del covAList[lineNumber]
+            del covBList[lineNumber]    
+
     print(covAList) 
     print(covBList) 
 
