1cbb43d32359a875ff9ca444bccac0886be79a1c
Enrico <enrivigano@gmail.com>
2021-04-06 15:12:10 +0200

    removing "singleton" options

--- mutator/python/executeTest.sh
+++ mutator/python/executeTest.sh
@@ -19,17 +19,17 @@ rm $outFile
 
 pwd
 
-if [[ -z _FAQAS_SINGLETON_FM ]]; then
-    extra=""
-else
-    extra="-D_FAQAS_SINGLETON_FM"
-fi
+# if [[ -z _FAQAS_SINGLETON_FM ]]; then
+#     extra=""
+# else
+#     extra="-D_FAQAS_SINGLETON_FM"
+# fi
 
 operations=`grep 'MUTATIONOPT=' FAQAS_dataDrivenMutator.h | tr '/' ' ' | awk -F= '{print $2}'`
 echo $operations
 x=-1
 while [ $x -le $operations ]; do
-    g++ $extra -DMUTATIONOPT=$x ${curTest}.c -o main_$x >> $compilerOutFile 2>&1
+    g++ -DMUTATIONOPT=$x ${curTest}.c -o main_$x >> $compilerOutFile 2>&1
     ./main_$x >> $outFile 2>&1
     echo "=====" >> $outFile 2>&1
 
--- mutator/python/runTests.sh
+++ mutator/python/runTests.sh
@@ -1,8 +1,5 @@
 #/bin/bash
 
-#The following is necessary to use the singletn mode
-#export _FAQAS_SINGLETON_FM=True
-
 bash cleanTests.sh
 
 pushd tests
