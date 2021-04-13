3a0514d3d3e826386d0366bc59ba80754b8e07ed
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-04-14 00:51:54 +0200

    adding case study

---
+++ case_studies/ASN/README.md
@@ -0,0 +1,4 @@
+
+- Download ASN folder as zip from [https://dropit.uni.lu/invitations/?share=b5654d5f4e23cc9020f2#](https://dropit.uni.lu/invitations/?share=b5654d5f4e23cc9020f2#)
+
+- Extract it into folder `DOWNLOADED`
---
+++ case_studies/ASN/scripts/create_mutants.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export SRCIROR=/opt/srcirorfaqas
+export LLVM_BUILD=${SRCIROR}/llvm-build
+
+export SRCIROR_LLVMMutate_LIB=${LLVM_BUILD}/Release+Asserts/lib/LLVMMutate.so
+export SRCIROR_SRC_MUTATOR=${SRCIROR}/SRCMutation/build/mutator
+export SRCIROR_LLVM_BIN=${LLVM_BUILD}/Release+Asserts/bin/
+export SRCIROR_LLVM_INCLUDES=${LLVM_BUILD}/Release+Asserts/lib/clang/3.8.0/include/
+export SRCIROR_COVINSTRUMENTATION_LIB=${SRCIROR}/InstrumentationLib/SRCIRORCoverageLib.o
+
+#variables
+SRCIROR_COMPILER=/opt/srcirorfaqas/PythonWrappers/mutationClang
+SRC_MUTANTS=/opt/mutations/grammar_mutants
+
+mkdir -p $SRC_MUTANTS
+
+#modify accordingly
+PATH_TO_SRC=$HOME/grammar_test
+
+LOGFILE=$SRC_MUTANTS/mutation`date +"%Y%m%d%H%M"`.log
+touch $LOGFILE
+
+trap "exit" INT
+jq -c '.[]' compile_commands.json | while read i; do
+	FILE=`echo $i | jq -r '.file'`
+	ARGS=`echo $i | jq '.command'`
+	DIR=`echo $i | jq -r '.directory'`
+	
+	echo "-----------------------" 2>&1 | tee -a $LOGFILE
+	echo "Mutating "$FILE 2>&1 | tee -a $LOGFILE
+
+	cd $DIR
+
+	path_wo_ext="${FILE%.*}"
+	path_wo_root="${path_wo_ext#$PATH_TO_SRC}"
+	
+	mkdir -p $SRC_MUTANTS/$path_wo_root
+	
+	orig_dir=`dirname -- "$FILE"`
+
+	ARGS_wo_quotes=`sed -e 's/^"//' -e 's/"$//' <<<"$ARGS"`
+
+	echo ----------------	
+	echo $ARGS_wo_quotes
+	echo ---------------
+		
+	$SRCIROR_COMPILER --compilation "$ARGS_wo_quotes" 2>&1 | tee -a $LOGFILE
+	mv $orig_dir/*.mut.*.c $SRC_MUTANTS/$path_wo_root
+
+done
