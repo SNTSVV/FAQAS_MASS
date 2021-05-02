478100af9dd3325eb3e3f0a1f136cb61e568f191
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-05-02 11:03:34 +0000

    Update case_studies/ASN/scripts/run.sh

--- case_studies/ASN/scripts/run.sh
+++ case_studies/ASN/scripts/run.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 set -u
-set -o pipefail
 
 TOPDIR=$(dirname $(readlink -f $0))
 filename=$(basename $0)
@@ -123,7 +122,7 @@ if [ $phase -le 2 ]; then
         $build_bc_func $meta_mutant_src_file $meta_mutant_bc_file || error_exit "Building bc file failed"
     else
         echo "[$filename] Switching to docker..."
-        $cd_docker_script $ws_dir_here "$in_docker_cmd pre-semu" 2>&1 | sed "s|$ws_in_docker|$ws_dir_here|g" || error_exit "Failure in docker"
+        (set -o pipefail && $cd_docker_script $ws_dir_here "$in_docker_cmd pre-semu" 2>&1 | sed "s|$ws_in_docker|$ws_dir_here|g") || error_exit "Failure in docker"
     fi
 fi
 
@@ -134,7 +133,7 @@ if [ $phase -le 3 ]; then
         $tool_dir/underlying_test_generation/main.py $meta_mutant_bc_file --output_top_directory $gen_test_dir --clear_existing --generation_timeout $gen_timeout || error_exit "Test generation failed"
     else
         echo "[$filename] Switching to docker..."
-        $cd_docker_script $ws_dir_here "$in_docker_cmd testgeneration" 2>&1 | sed "s|$ws_in_docker|$ws_dir_here|g" || error_exit "Failure in docker"
+        (set -o pipefail && $cd_docker_script $ws_dir_here "$in_docker_cmd testgeneration" 2>&1 | sed "s|$ws_in_docker|$ws_dir_here|g") || error_exit "Failure in docker"
     fi
 fi
 
