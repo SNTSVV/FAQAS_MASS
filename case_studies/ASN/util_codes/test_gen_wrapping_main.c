061ce5551355ad4b8bbc48feaf3a3c76d3dc6f9d
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-05-02 13:42:44 +0000

    Update case_studies/ASN/util_codes/test_gen_wrapping_main.c

--- case_studies/ASN/util_codes/test_gen_wrapping_main.c
+++ case_studies/ASN/util_codes/test_gen_wrapping_main.c
@@ -1,10 +1,14 @@
 
 /* Append this to the generate meta-mu source code to create the <name>.MetaMu.MakeSym.c*/
 
+#include "test_auto_tcs.c"
+
 #include "klee/klee.h"
 
 int main(int argc, char** argv)
 {
+    (void)argc;
+    (void)argv;
     flag result;
     int errCode;
     MyInt tmp0;
@@ -14,6 +18,6 @@ int main(int argc, char** argv)
 
     MyInt_Initialize((&(tmp0)));
     result = MyInt_enc_dec(&tc_data, &errCode, "test_case_UPER_*");
-    return int(result);
+    return (int)result;
 }
 
