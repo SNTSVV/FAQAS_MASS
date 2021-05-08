3f18ab28eb24fc77b1717e181ba5c33945174a91
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-05-08 22:14:14 +0200

    add second int

---
+++ case_studies/ASN/util_codes/direct/My2ndInt_Encode.wrapping_main.c
@@ -0,0 +1,25 @@
+
+/* Append this to the generate meta-mu source code to create the <name>.MetaMu.MakeSym.c*/
+
+#include "asn1crt.c"
+#include "asn1crt_encoding.c"
+#include "asn1crt_encoding_uper.c"
+
+#include "klee/klee.h"
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+    flag result;
+    My2ndInt val0;
+    BitStream bitStrm;
+    flag checkC;
+    int errCode;
+    klee_make_symbolic(&val0, sizeof(val0), "val0");
+    klee_make_symbolic(&bitStrm, sizeof(bitStrm), "bitStrm");
+    klee_make_symbolic(&checkC, sizeof(checkC), "checkC");
+
+    result = My2ndInt_Encode(&val0, &bitStrm, &errCode, checkC);
+    return (int)result;
+}
---
+++ case_studies/ASN/util_codes/direct/My2ndInt_IsConstraintValid.wrapping_main.c
@@ -0,0 +1,23 @@
+
+/* Append this to the generate meta-mu source code to create the <name>.MetaMu.MakeSym.c*/
+
+#include "asn1crt.c"
+#include "asn1crt_encoding.c"
+#include "asn1crt_encoding_uper.c"
+
+#include "klee/klee.h"
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+    flag result;
+    My2ndInt val0;
+    int errCode;
+    klee_make_symbolic(&val0, sizeof(val0), "val0");
+
+    result = My2ndInt_IsConstraintValid(&val0, &errCode);
+    return (int)result;
+}
+
+
