eadf8a7343c962388d587e525d5759a7e9850416
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-06-13 13:14:53 +0000

    Update case_studies/ASN/util_codes/generate_direct.py
    Former-commit-id: df4863d4ad916ac1302e634212dd1fe6057a76d3

--- case_studies/ASN/util_codes/generate_direct.py
+++ case_studies/ASN/util_codes/generate_direct.py
@@ -4,14 +4,54 @@
 
 import os
 import argparse
+from jinja2 import Template
 
 import clang.cindex
+
+USED_TEMPLATE = """
+/* Append this to the generate meta-mu source code to create the <name>.MetaMu.MakeSym.c*/
+
+#include "asn1crt.c"
+#include "asn1crt_encoding.c"
+#include "asn1crt_encoding_uper.c"
+
+#include "klee/klee.h"
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+    {{ function_return_type }} result_faqas_semu;
+{% for arg_ptr_stripped_decl in arg_ptr_stripped_decl_list %}
+    {{ arg_ptr_stripped_decl }};
+{% endfor %}
+{% for arg_name in input_arg_name_list %}
+    klee_make_symbolic(&{{ arg_name }}, sizeof({{ arg_name }}), "{{ arg_name }}");
+{% endfor %}}
+
+    result_faqas_semu = {{ function_name }}}({{ call_args_list|join(", ") }});
+    {{ output_out_arg }}
+    return {{ result_faqas_semu_to_int }};
+}
+
+"""
 
 class Prototype:
     return_type = None
     function_name = None
     # list of tuples of: def type
     params_type_name = []
+
+    @staticmethod
+    def type_is_ptr(arg_decl):
+        return '*' in arg_decl
+
+    @staticmethod
+    def get_ptr_stripped(arg_decl):
+        last_star_index = arg_decl.rfind("*")
+        if last_star_index < 0:
+            return arg_decl
+        return arg_decl[:last_star_index] + ' ' + arg_decl[last_star_index + 1:]
 
 def get_prototype(func_decl):
     pass
@@ -32,8 +72,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("compilation_db", metavar="compilation-db", help="Compilation database file")
     parser.add_argument("source_file", metavar="source-file", help="source file containing the functions to test (all defined functions)")
+    parser.add_argument("output_dir", metavar="output-dir", help="Directory to put the generated files")
     args = parser.parse_args()
+    if not os.path.join(args.compilation_db):
+        assert False, "The specified compilation_db file does not exist"
+    if not os.path.join(args.source_file):
+        assert False, "The specified source file does not exist" 
     prototypes = get_function_prototypes(args.source_file, args.compilation_db)
+
+    if not os.path.join(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    for prototype in prototypes:
+        code_filepath = os.path.join(args.output_dir, prototype.function_name + '.' + "wrapping_main.c")
+        code = Template(USED_TEMPLATE).render(
+            something="World"
+        )
+        with open(code_filepath) as f:
+            f.write(code)
+    
+    print("Done writing templates in folder {}".format(args.output_dir))
 
 if __name__ == "__main__":
     main()
