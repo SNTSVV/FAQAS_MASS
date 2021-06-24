097447ca4ab8b1df5dd746e17b2a36cfdc549695
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-06-24 13:16:28 +0200

    update templates after fix
    Former-commit-id: b2269d071400f6acec3221f0bae2e72831ab3898

--- case_studies/ASN/util_codes/direct/My2ndBool_IsConstraintValid.wrapping_main.c
+++ case_studies/ASN/util_codes/direct/My2ndBool_IsConstraintValid.wrapping_main.c
@@ -1,5 +1,5 @@
 
-/* Wrapping main template for the function My2ndBool_IsConstraintValid defined in the file ../DOWNLOADED/casestudy/test.c
+/* Wrapping main template for the function My2ndBool_IsConstraintValid defined in the file ../DOWNLOADED/casestudy/test.c */
 /* Append this to the generate meta-mu source code to create the <name>.MetaMu.MakeSym.c */
 
 #include <stdio.h>
--- case_studies/ASN/util_codes/direct/TypeNested_label_Equal.wrapping_main.c
+++ case_studies/ASN/util_codes/direct/TypeNested_label_Equal.wrapping_main.c
@@ -1,5 +1,5 @@
 
-/* Wrapping main template for the function TypeNested_label_Equal defined in the file ../DOWNLOADED/casestudy/test.c
+/* Wrapping main template for the function TypeNested_label_Equal defined in the file ../DOWNLOADED/casestudy/test.c */
 /* Append this to the generate meta-mu source code to create the <name>.MetaMu.MakeSym.c */
 
 #include <stdio.h>
