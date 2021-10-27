1435f2de36b163e1af0dad482ea0997d6a665d7c
Titcheu Chekam Thierry <thierry_tct@yahoo.com>
2021-10-27 11:13:21 +0200

    Update install_requirements.sh
    Former-commit-id: 8fa8897f08c37de7f6f9255266a6af4f038f8105

--- install_requirements.sh
+++ install_requirements.sh
@@ -17,14 +17,15 @@ TOPDIR=$(dirname $(readlink -f $0))
 ## envure python version
 python -c "import platform; exit(int(not (platform.python_version_tuple() > ('3','4'))))" || error_exit "Python version too low"
 
-## install requirements
-pip install -r $TOPDIR/requirements.txt
-
 # Install system packages (case study dependencies)
 if [ $(id -u) -eq 0 ]; then
     # already sudo
     apt-get -y install jq
+    apt-get -y install libjpeg-dev zlib1g-dev
 else
     sudo apt-get -y install jq
+    sudo apt-get -y install libjpeg-dev zlib1g-dev
 fi
 
+## install requirements
+pip install -r $TOPDIR/requirements.txt
